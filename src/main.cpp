ee1d6eeee7eefddc73ca93ef3ab69b4f0e8a3bb8
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2022-06-06 01:33:51 +0500

    completed

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #endif
 #include "addons/RTDBHelper.h"
 #include <Firebase_ESP_Client.h>
+#include "time.h"
 // Provide the token generation process info.
 #include <addons/TokenHelper.h>
 // Provide the RTDB payload printing info and other helper functions.
@@ -21,6 +22,8 @@
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
 #define USER_EMAIL "esp32firebase@gmail.com"
 #define USER_PASSWORD "password123"
+const String timePath = "timestamp";
+const char *ntpServer = "pool.ntp.org";
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <FirebaseJson.h>
@@ -38,7 +41,7 @@ static const int servoPin = 26;
 
 Servo myservo;
 unsigned long sendDataPrevMillis = 0;
-
+unsigned long getTime();
 unsigned long count = 0;
 float currentTemperature;
 float desiredT;
@@ -55,7 +58,7 @@ void feedNowRoutine();
 void setup()
 {
   pinMode(heaterPin, OUTPUT);
-  digitalWrite(heaterPin, HIGH); 
+  digitalWrite(heaterPin, HIGH);
   Serial.begin(115200);
   sensors.begin();
   ESP32PWM::allocateTimer(0);
@@ -64,11 +67,7 @@ void setup()
   ESP32PWM::allocateTimer(3);
   myservo.setPeriodHertz(50);           // standard 50 hz servo
   myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
-  myservo.write(0);
-  delay(1000);
-  myservo.write(90);
-  delay(2000);
-  myservo.write(0); 
+  myservo.write(180);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -94,6 +93,7 @@ void setup()
   Firebase.reconnectWiFi(true);
 
   Firebase.setDoubleDigits(5);
+  configTime(18000, 0, ntpServer);
 }
 unsigned long ms = 0;
 
@@ -102,8 +102,18 @@ void loop()
   sensors.requestTemperatures();
   currentTemperature = sensors.getTempCByIndex(0);
   Serial.println(currentTemperature);
+  if (currentTemperature < desiredT)
+  {
+    digitalWrite(heaterPin, LOW); // turn it on
+  }
+  if (currentTemperature > desiredT)
+  {
+    digitalWrite(heaterPin, HIGH);
+  }
   if (millis() - ms >= 1000)
   {
+    int timestamp = getTime();
+    Serial.println(timestamp);
     ms = millis();
     if (Firebase.RTDB.getInt(&fbdo, "heaterState"))
     {
@@ -129,9 +139,10 @@ void loop()
       Serial.println("REASON: " + fbdo.errorReason());
     }
 
-    if (Firebase.RTDB.getFloat(&fbdo, "desiredT"))
+    if (Firebase.RTDB.getString(&fbdo, "desiredT"))
     {
-      desiredT = fbdo.to<float>();
+      String data = fbdo.to<String>();
+      desiredT = data.toFloat();
       Serial.print("OK: desired temperature: ");
       Serial.println(desiredT);
     }
@@ -141,9 +152,11 @@ void loop()
       Serial.println("REASON: " + fbdo.errorReason());
     }
 
-    if (Firebase.RTDB.getInt(&fbdo, "feedInterval"))
+    if (Firebase.RTDB.getString(&fbdo, "feedInterval"))
     {
-      feedInterval = fbdo.to<float>();
+      String data = fbdo.to<String>();
+
+      feedInterval = data.toFloat();
       Serial.print("OK: feedInterval: ");
       Serial.println(feedInterval);
     }
@@ -163,26 +176,20 @@ void loop()
       Serial.println("REASON: " + fbdo.errorReason());
     }
   }
-
-  if (currentTemperature < desiredT)
-    digitalWrite(heaterPin, LOW); // turn it on
-
-  if (currentTemperature > desiredT)
-    digitalWrite(heaterPin, HIGH);
-
   if (feedNow)
   {
     feedNowRoutine();
     feedNow = false;
-    if (Firebase.RTDB.setFloat(&fbdo, "feedNow", 0))
+    if (Firebase.RTDB.setFloat(&fbdo, "feedNow", feedNow))
     {
-      Serial.println("Successfully updated feednow to false");
+      Serial.println("Successfully updated feednow");
     }
     else
     {
       Serial.println("ERROR");
     }
     Serial.println("Feednow routine finished.");
+    lastFeed = millis();
   }
 
   if (heaterState == 1)
@@ -194,7 +201,7 @@ void loop()
     digitalWrite(heaterPin, HIGH);
   }
 
-  if ((feedInterval != 0) && (millis() - lastFeed >= feedInterval * 1000)) // replace by 1000 if s
+  if ((feedInterval != 0) && (millis() - lastFeed >= feedInterval * 1000) && (feedNow == 0)) // replace by 1000 if s
   {
     lastFeed = millis();
     Serial.println("Feeding according to interval");
@@ -202,13 +209,29 @@ void loop()
   }
 }
 
+// Function that gets current epoch time
+unsigned long getTime()
+{
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    // Serial.println("Failed to obtain time");
+    return (0);
+  }
+  time(&now);
+  return now;
+}
 void feedNowRoutine()
 {
   // run servo to feeding position and bring it back.
   Serial.println("Feeding now.... ");
+
+  // Firebase.RTDB.pushString(&timePath, &timestamp);
+
   myservo.write(180);
   delay(2000);
   myservo.write(0);
-  
+  delay(2000);
+  myservo.write(180);
 }
-
